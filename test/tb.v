0679848190d45576a2bbcaab6bdcb2f977a72235
workfefra <privat.felixfranz@gmail.com>
2025-11-08 17:45:58 +0100

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,49 +1,28 @@
 `default_nettype none
 `timescale 1ns / 1ps
 
-/* This testbench just instantiates the module and makes some convenient wires
-   that can be driven / tested by the cocotb test.py.
-*/
-module tb ();
+module tb_tt_um_endecoder_workfefra;
 
-  // Dump the signals to a VCD file. You can view it with gtkwave or surfer.
-  initial begin
-    $dumpfile("tb.vcd");
-    $dumpvars(0, tb);
-    #1;
-  end
+  reg clk = 0;
+  always #5 clk = ~clk;
 
-  // Wire up the inputs and outputs:
-  reg clk;
-  reg rst_n;
-  reg ena;
-  reg [7:0] ui_in;
-  reg [7:0] uio_in;
+  reg [7:0] ui_in = 0;
   wire [7:0] uo_out;
-  wire [7:0] uio_out;
-  wire [7:0] uio_oe;
-`ifdef GL_TEST
-  wire VPWR = 1'b1;
-  wire VGND = 1'b0;
-`endif
 
-  // Replace tt_um_example with your module name:
-  tt_um_example user_project (
-
-      // Include power ports for the Gate Level test:
-`ifdef GL_TEST
-      .VPWR(VPWR),
-      .VGND(VGND),
-`endif
-
-      .ui_in  (ui_in),    // Dedicated inputs
-      .uo_out (uo_out),   // Dedicated outputs
-      .uio_in (uio_in),   // IOs: Input path
-      .uio_out(uio_out),  // IOs: Output path
-      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-      .ena    (ena),      // enable - goes high when design is selected
-      .clk    (clk),      // clock
-      .rst_n  (rst_n)     // not reset
+  tt_um_endecoder_workfefra uut (
+    .ui_in(ui_in),
+    .uo_out(uo_out),
+    .clk(clk),
+    .rst_n(1'b1),
+    .ena(1'b1),
+    .uio_in(8'h00),
+    .uio_out(),
+    .uio_oe()
   );
+
+  initial begin
+    $display("Testbench running OK");
+    #100 $finish;
+  end
 
 endmodule
